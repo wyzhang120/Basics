61606ac3ff7f9c9d12e1e27ce76e216b0b4d9764
Wenyuan_AGL <wyzhang120@gmail.com>
2020-01-09 12:41:31 -0600

    comments added

--- straight_ray.py
+++ straight_ray.py
@@ -4,9 +4,13 @@ from scipy.sparse.linalg import inv, spsolve
 import matplotlib.pyplot as plt
 import h5py
 
+"""
+    Straing ray tomo in crosswell geometry. src on the left and rec on the right.
+"""
 par = {'length': 300., 'height': 105., 'dx': 5., 'dz': 5.,
        'xsrc': 0., 'osV': 0., 'dsrc': 1., 'bsV': 105.5,
-       'xrec': 300, 'drec': 1., 'orV': 0., 'brV': 105.5, }
+       'xrec': 300, 'drec': 1., 'orV': 0., 'brV': 105.5,
+       'vp_max': 5800, 'vp_min': 2500, 'damping': 0.5}
 
 par['nx'] = int(par['length'] / par['dx'] + 1.5)
 par['nz'] = int(par['height'] / par['dz'] + 1.5)
@@ -15,12 +19,25 @@ zrec = np.arange(par['orV'], par['brV'], par['drec'])
 nsrc = len(zsrc)
 nrec = len(zrec)
 
-# time table shape = (106, 106)
+# Read time table shape = (106, 106)
 tt = np.fromfile('reco_rV2.rsf@', dtype=np.float32).reshape([-1, 1])
 
 
 def ray_seg_len(xzsrc, xzrec, nx, nz, dx=1., dz=1.,
                 zmin=0., xmin=0.,):
+    """
+    Given src and rec coords, compute length ray of segments in cells traveled through.
+    :param xzsrc: tuple, float
+    :param xzrec: tuple, float
+    :param nx: int, model size
+    :param nz: int,
+    :param dx: float
+    :param dz: float
+    :param zmin: float, model z bound
+    :param xmin: float,
+    :return:
+        vec: 1d array, shape = nx * nz. Assuming fast dim = z
+    """
     xmax = (nx - 1) * dx + xmin
     zmax = (nz - 1) * dz + zmin
     xgrid = np.arange(xmin - dx/2, xmax + dx, dx)
@@ -54,13 +71,10 @@ def ray_seg_len(xzsrc, xzrec, nx, nz, dx=1., dz=1.,
         ray_len = np.vstack((ray_len.reshape([-1, 1]), np.array([max_len]).reshape([1, 1])))
         xz = np.vstack((xz, xzrec))
         id_max = len(ray_len) - 1
-
     ray_len[id_max] = max_len
     xz[id_max] = xzrec
     ray_len = ray_len[:id_max+1]
     xz = xz[:id_max+1]
-    # xz = np.vstack((xzsrc, xz))
-    # ray_len = np.vstack((np.zeros([1, 1]), ray_len.reshape([-1, 1])))
     dxz = np.array((dx, dz))
     ids = np.int32((xz - xz0) / dxz)
     ids[:, 0] = np.clip(ids[:, 0], 0, nx-1)
@@ -83,15 +97,14 @@ for isrc in range(nsrc):
                                     par['dx'], par['dz'])
 
 
-damping = 0.5
 G = sparse.csr_matrix(raymat)
 GTG = G.T.dot(G)
 R = sparse.diags(1./GTG.diagonal())
-op = R.dot(GTG) + damping * sparse.eye(G.shape[1])
+op = R.dot(GTG) + par['damping'] * sparse.eye(G.shape[1])
 rhs = R.dot(G.T).dot(tt)
 slowness = spsolve(op, rhs)
 vp_inv = 1./slowness.reshape([par['nx'], par['nz']])
-vp_inv = np.clip(vp_inv, 2500, 5800)
+vp_inv = np.clip(vp_inv, par['vp_min'], par['vp_max'])
 fig, ax = plt.subplots()
 im = ax.imshow(vp_inv.T, extent=(
     -par['dx']/2., par['length']+par['dx']/2,
